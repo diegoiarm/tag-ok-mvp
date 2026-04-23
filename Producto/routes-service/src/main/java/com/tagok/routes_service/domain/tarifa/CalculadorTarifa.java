57324d709c91b6aeff9ffc26f6093c44bce03603
Roony11-1 <eltheasasin@gmail.com>
2026-04-23 14:34:28 -0400

    Actualizado el metodo para solo calcular el valor y la informacion del cruce por un portico

--- Producto/routes-service/src/main/java/com/tagok/routes_service/domain/tarifa/CalculadorTarifa.java
+++ Producto/routes-service/src/main/java/com/tagok/routes_service/domain/tarifa/CalculadorTarifa.java
@@ -2,21 +2,30 @@ package com.tagok.routes_service.domain.tarifa;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.tagok.routes_service.domain.portico.Portico;
 import com.tagok.routes_service.domain.vehiculo.TipoVehiculo;
 
 public class CalculadorTarifa
 {
-    public TarifaCalculada calcular(
+    public Cruce calcular(
         Portico portico,
         TipoVehiculo vehiculo,
         LocalDateTime fechaHora)
     {
-        BigDecimal monto = portico.calcularTarifa(vehiculo, fechaHora);
+        Tarifa tarifa = portico.calcularTarifa(vehiculo, fechaHora);
 
-        var cruce = new Cruce(portico.getId(), portico.getCodigo(), monto);
+        var cruce = new Cruce(
+            portico.getId(), 
+            portico.getCodigo(), 
+            portico.getNombre(),
+            portico.getAutopista().getNombre(),
+            tarifa.tipoTarifa(),
+            tarifa.monto(),
+            fechaHora);
 
-        return new TarifaCalculada(monto, cruce, fechaHora);
+        return cruce;
     }
 }
